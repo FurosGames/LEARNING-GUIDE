ce3ad5057a0902e4a7bd1e7604b75575d7453140
luucas-0 <luucaas.ahs@gmail.com>
2026-04-16 18:09:02 -0500

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/cohorte5/Escritorio/shmueldev/Aprender/guia_aprendizaje/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/cohorte5/Escritorio/shmueldev/Aprender/guia_aprendizaje/node_modules/infima/dist/css/default/default.css"),
+  require("/home/cohorte5/Escritorio/shmueldev/Aprender/guia_aprendizaje/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/cohorte5/Escritorio/shmueldev/Aprender/guia_aprendizaje/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/cohorte5/Escritorio/shmueldev/Aprender/guia_aprendizaje/src/css/custom.css"),
+];
